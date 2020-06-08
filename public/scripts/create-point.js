3fa4c7ad38b2ddaaad0372450eb03beb9b06b695
Eder-R <eder.ramos@catolicasc.org.br>
2020-06-07 23:42:46 -0300

    Site completo

---
+++ public/scripts/create-point.js
@@ -0,0 +1,104 @@
+
+
+function populateUFs()  {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( (res) => { return res.json() } )// tbm da para fazer assim   ( res => res.json() )
+    .then( states =>{
+
+        for(const state of states)   {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+
+    } )
+}
+
+populateUFs()
+
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+    const ufValue = event.target.value
+
+    
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( (res) => { return res.json() } )// tbm da para fazer assim   ( res => res.json() )
+    .then( cities =>{
+
+        for(const city of cities)   {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    } )
+}
+
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change",getCities)
+
+  
+//Itens de coleta
+//Pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click",handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //adicionar ou remover uma classe com Javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    console.log('Item id:   ',itemId)
+
+    //Verificar se existem  items selecionados,se sim
+    //pegar os items selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId
+        return itemFound
+    } )
+
+    //se ja estiver selecionada,
+    if(alreadySelected >=0 ){
+        //tirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item!=itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    }else {
+        //se não estiver selecionado, 
+        //adicionar à seleção
+        selectedItems.push(itemId)
+    }
+
+    console.log('selectedItems: ', selectedItems)   
+
+    //atualizar o campo escondido com os items selecionados
+    collectedItems.value = selectedItems
+
+}
---
+++ src/server.js
@@ -0,0 +1,111 @@
+const express = require("express")
+const server = express()
+
+//Pegar o banco de dados
+const db = require("./database/db")
+
+//  Configurar a pasta public
+server.use(express.static("public"))
+
+// Habilitar o  uso    do req.body na nossa aplicação
+server.use(express.urlencoded({ extended:true }))
+
+
+//  Utilizando  template engine 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//  Configurar caminhos da minha aplicação
+//  Página Inicial
+//  req: Requisição
+//  res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", {title:"Um titulo"})
+})
+
+
+
+server.get("/create-point", (req, res) => {
+    
+
+    // rew.query:Query strings da nossa url
+    //console.log(req.query)
+
+    
+    return res.render("create-point.html")
+})
+
+server.post("/savepoint",(req, res) => {
+    
+    //req.body: O corpo do nosso formulario
+    //console.log(req.body)
+
+    // inserir dados no banco de dados
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+  
+  function afterInsertData(err) {
+    if(err){
+        console.log(err)
+        return res.send("Erro no cadastro!")
+    }
+
+    console.log("Cadastrado com Sucesso")
+    console.log(this)
+
+    return res.render("create-point.html", {saved:true})
+}
+
+     db.run(query, values, afterInsertData)
+    
+    
+})
+
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+    
+    if (search == "")  {
+        //pesquisa vazia
+        return res.render("search-results.html", {total:0})
+    }
+
+    // pegar os dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) { //usa-se * para selecionar todos
+        if(err){
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        // mostra a página HTML com os dados do banco de dados
+        return res.render("search-results.html", {places: rows, total:total})
+    })   
+})
+
+
+//  Ligar o servidor
+server.listen(3000)
